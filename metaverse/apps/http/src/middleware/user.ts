a46f4dbdeb0caed57f0a99f8fc68214348065a4a
Jafrash <jafrash123@gmail.com>
2025-06-27 20:57:46 +0530

    authenticate

--- metaverse/apps/http/src/middleware/user.ts
+++ metaverse/apps/http/src/middleware/user.ts
@@ -6,7 +6,7 @@ import { NextFunction, Request, Response } from "express";
 export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
     // Try to get token from Authorization header first
     const header = req.headers["authorization"];
-    const token = header?.split(" ")[1] || header;  // Also allow raw token in header
+    let token = header?.split(" ")[1] || header;  // Also allow raw token in header
     
     // If no token in header, try to get it from the request body
     if (!token && req.body && typeof req.body === 'object') {
